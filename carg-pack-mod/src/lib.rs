a6cabf2a1fa565e26f272756ae5e3c2db2b85df5
vanshavenger <vanshchopra101@gmail.com>
2024-09-24 21:28:43 +0530

    pachakes

--- carg-pack-mod/src/lib.rs
+++ carg-pack-mod/src/lib.rs
@@ -2,22 +2,71 @@
 
 #![allow(dead_code, unused_variables)]
 
-mod database;
+// mod database;
 
-pub mod balle_balle_auth_ki_utilities;
+// pub mod balle_balle_auth_ki_utilities;
 
-use balle_balle_auth_ki_utilities::login;
-use balle_balle_auth_ki_utilities::models::Cred;
-use database::{connect_to_db, Status};
+// use balle_balle_auth_ki_utilities::login;
+// use balle_balle_auth_ki_utilities::models::Cred;
+// use database::{connect_to_db, Status};
 
-pub fn authenticate(cred: &Cred) {
-    if let Status::CONNECTED = connect_to_db() {
-        login(cred);
+// pub fn authenticate(cred: &Cred) {
+//     if let Status::CONNECTED = connect_to_db() {
+//         login(cred);
 
-        println!("User connected");
-    } else {
-        println!("User not connected");
+//         println!("User connected");
+//     } else {
+//         println!("User not connected");
+//     }
+// }
+
+// pub mod util;
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+
     }
 }
 
-pub mod util;
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+}
+
+fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please", meal.toast);
+}
--- carg-pack-mod/src/main.rs
+++ carg-pack-mod/src/main.rs
@@ -1,14 +1,28 @@
-use carg_pack_mod::{authenticate, balle_balle_auth_ki_utilities::models::Cred, util};
+// use carg_pack_mod::{authenticate, balle_balle_auth_ki_utilities::models::Cred, util};
+use crate::garden::vegetables::Asparagus;
+
+use std::io::Result;
+use std::fmt::Result as FmtResult;
+
+pub mod garden;
+
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    let user = Cred {
-        username: String::from("admin"),
-        password: String::from("admin"),
-    };
+    // let user = Cred {
+    //     username: String::from("admin"),
+    //     password: String::from("admin"),
+    // };
 
-    authenticate(&user);
+    // authenticate(&user);
 
-    util::op();
+    // util::op();
+
+    let plant = Asparagus::TYPEA;
+    println!("{:?}", plant);
+
+    
+
+    
 }
