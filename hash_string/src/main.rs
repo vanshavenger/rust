54f5aa357e9dcd7f5a9d6dd14972bf0e163f3a32
vanshavenger <vanshchopra101@gmail.com>
2024-10-02 15:29:52 +0530

    hashmap

--- hash_string/src/main.rs
+++ hash_string/src/main.rs
@@ -1,4 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
+use std::collections::HashMap;
 
 fn main() {
 
@@ -53,6 +54,57 @@ fn main() {
     } else {
         println!("Failure");
     }
+
+
+    //Hashmap
+    let mut scores:HashMap<i32, i32> = HashMap::new();
+
+    scores.insert(1, 10);
+    scores.insert(2, 20);
+    
+    println!("{:?}", scores);
+
+
+    let team_name = String::from("Blue");
+    let team_score = 10;
+
+    let mut team = HashMap::new();
+    team.insert(team_name, team_score);
+
+    println!("{:?}", team);
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{}", score);
+
+    scores.insert(String::from("Blue"), 100);
+
+    println!("{:?}", scores);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+
+
+
 
 
 
